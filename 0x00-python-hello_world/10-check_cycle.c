a902341319b712e7ce5c0bbd236f8c16bef5e36f
keira-claudette <claudette.keira@gmail.com>
2021-04-26 17:57:38 +0000

    first commit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: pointer to the linked list
+ * Return: Returns 0 to indicate no cycle  and 1 to indicate cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *temp = list;
+
+	while (temp)
+	{
+		temp = temp->next;
+
+		if (temp == list)
+			break;
+			return (1);
+	}
+	if (temp == NULL)
+		return (0);
+
+	return (1);
+}
