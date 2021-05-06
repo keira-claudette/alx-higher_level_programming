727f58bdeb14e35ec83a6dfab8d043b4fb86af0f
keira-claudette <claudette.keira@gmail.com>
2021-05-06 16:58:38 +0000

    defined function to replace lambda

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    power_2 = lambda x: x ** 2
+    def power_2(x):
+        return x ** 2
+
     squares = []
     for i in matrix:
         squares.append(list(map(power_2, i)))
