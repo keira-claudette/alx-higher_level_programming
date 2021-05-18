1499eaea8947d96f2f96a040b80761213bf33b3d
keira-claudette <claudette.keira@gmail.com>
2021-05-18 02:02:53 +0000

    added format specifier

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for i in my_list:
-            print('{}'.format(my_list[i - 1]), end='')
+            print('{:d}'.format(my_list[i - 1]), end='')
             if i == x:
                 break
     except:
