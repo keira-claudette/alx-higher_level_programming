ea641262dc6d175976f41774b7b0b240ed17a581
keira-claudette <claudette.keira@gmail.com>
2021-05-19 15:20:22 +0000

    docstring added

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 class Square:
-    """a class Square that defines a square by: (based on 1-square.py)
+    """ Represents a Square that defines a square by: (based on 1-square.py)
+    Private instance attribute: size.
+    Instantiation with optional size.
     """
     def __init__(self, size=0):
+        """ Initializes the data."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
