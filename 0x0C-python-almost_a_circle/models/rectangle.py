dd2488bc5e768d086cfcaae4346eec95573e31e6
keira-claudette <claudette.keira@gmail.com>
2021-06-08 18:33:41 +0000

    fixed error

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,7 +84,7 @@ class Rectangle(Base):
             print()
         for i in range(self.__height):
             for x in range(self.__x):
-                print("", end='')
+                print(" ", end="")
             for j in range(self.__width):
                 print("#", end="")
             print()
