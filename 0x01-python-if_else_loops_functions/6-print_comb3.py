7f213a8dada6ce013f3468527e67964f9b43cfdb
keira-claudette <claudette.keira@gmail.com>
2021-06-22 13:14:40 +0000

    added condition

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,5 +4,6 @@ This program prints all possible different combinations of two digits.
 """
 for i in range(0, 9):
     for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{:d}{}".format(i, j))
         print("{}{}".format(i, j), end=", ")
-print()
