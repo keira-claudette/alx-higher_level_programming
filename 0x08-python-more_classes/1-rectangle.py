2677c4ed2cd1e79289ae3227eff68174f8081b15
Claudette Mokeira <68082012+keira-claudette@users.noreply.github.com>
2021-06-23 20:04:55 +0300

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -40,7 +40,7 @@ class Rectangle:
         """ This assigns value to width"""
         if isinstance(value, int):
             if value < 0:
-                raise ValueError("width must be >=0")
+                raise ValueError("width must be >= 0")
             self.__width = value
         else:
             raise TypeError("width must be an integer")
@@ -55,7 +55,7 @@ class Rectangle:
         """ This assigns a new value to attribute height"""
         if isinstance(value, int):
             if value < 0:
-                raise ValueError("height must be >=0")
+                raise ValueError("height must be >= 0")
             self.__height = value
         else:
             raise TypeError("height must be an integer")
