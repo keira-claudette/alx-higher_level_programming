30dc0209cd40160381ebd8b6a695a977e5fbe515
keira-claudette <claudette.keira@gmail.com>
2021-07-14 12:45:19 +0000

    left-join tables

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,3 @@
+-- Lists all cities contained in the database hbtn_0d_usa.
+-- Each record displays cities.id - cities.name - states.name
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id;
