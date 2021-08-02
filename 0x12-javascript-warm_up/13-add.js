c21cfb14af53a4734cbcd7a27f3ecc9d652fecd1
keira-claudette <claudette.keira@gmail.com>
2021-08-02 19:57:48 +0000

    added test for task 13 and deleted reduntant bit of code in 13-add.js

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -8,6 +8,3 @@ http://51elliot.blogspot.com/2012/01/simple-intro-to-nodejs-module-scope.html
 exports.add = function (a, b) {
   return (a + b);
 };
-
-const add = require('./13-add').add;
-console.log(add(3, 5));
