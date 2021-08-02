cb2dc67102d80078e7eae7e1c7592a0e9f35663c
keira-claudette <claudette.keira@gmail.com>
2021-08-02 20:59:32 +0000

    advanced task: 100

---
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+Initial attempts were:
+                i) var myVar = 333; output was still 89
+                ii) const myVar = 333; output was still 89
+*/
+
+// ----- This function modifies the value of myVar to 333 -----
+
+myVar = 333;
