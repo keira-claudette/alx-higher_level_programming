e98d11eacda5d5460fce397331bacb735d43aea7
keira-claudette <claudette.keira@gmail.com>
2021-08-03 15:48:12 +0000

    added module.exports that was missing

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,8 +1,17 @@
 #!/usr/bin/node
 
-// Defines an empty class taht defines a rectangle
-class Rectangle {
-  constructor () {
-    console.log('[Function: Rectangle])
-  }
-};
+// Defines an empty class that defines a rectangle
+
+/*
+This tests for module dependecies. module.exports vs exports
+Excluding  module.exports from this module raises a TypeError exception:
+"Rectangle is not a constructor" when 0-main.js test is run.
+Running tests in 0-main.js as scripts in this module produces what's expected
+implying 0-main.js failing is an importing issue
+
+Read: https://www.sitepoint.com/understanding-module-exports-exports-node-js/
+*/
+
+class Rectangle {}
+
+module.exports = Rectangle;
