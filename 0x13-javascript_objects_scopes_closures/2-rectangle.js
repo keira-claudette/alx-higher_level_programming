6ba128c2492efbbc3c93a20d60e876e7f2a02f51
keira-claudette <claudette.keira@gmail.com>
2021-08-03 16:22:21 +0000

    initial commit

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// Describes a class that defines a rectangle
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
