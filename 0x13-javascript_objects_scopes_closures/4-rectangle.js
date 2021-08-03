291a3a8f870fb3588fd52892a020282323e9c9e1
keira-claudette <claudette.keira@gmail.com>
2021-08-03 17:43:15 +0000

    initial commit : task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,44 @@
+#!/usr/bin/node
+
+// ----- Describes a class that defines a rectangle -----
+
+/*
+Adds an instance method called rotate() that exchanges the width and the height
+ of the rectangle
+
+Adds an instance method called double() that multiples the width and the height
+ of the rectangle by 2
+*/
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let p = 'X';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width - 1; j++) {
+        p = p + 'X';
+      }
+      console.log(p);
+      p = 'X';
+    }
+  }
+
+  rotate () {
+    let temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
