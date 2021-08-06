ba62650cfaab09b64edb707fe957aaab042d518a
keira-claudette <claudette.keira@gmail.com>
2021-08-06 03:51:58 +0000

    initial commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+
+"""
+A script that lists all states from the database hbtn_0e_0_usa.
+
+1. Establish a connection to mysql server and select database
+2. Query the database
+3. List results
+
+"""
+
+db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+cursor = db.cursor()
+
+sql = "SELECT * FROM states ORDER BY states.id LIMIT 5"
+
+cursor.execute(sql)
+
+result = cursor.fetchall()
+
+for item in result:
+    print(item)
