33d8633700b1728431bc294952f78129faeead72
keira-claudette <claudette.keira@gmail.com>
2021-08-07 20:55:27 +0000

    imported modules within if __main__ statement

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,6 +14,9 @@ A script that lists all states from the database hbtn_0e_0_usa.
 """
 
 if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cursor = db.cursor()
