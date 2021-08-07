523869f714308e67f6905030cd7a6e7075b16c78
keira-claudette <claudette.keira@gmail.com>
2021-08-07 21:54:45 +0000

    added unique= and autoincrement= to Column

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -20,5 +20,6 @@ class State(Base):
     """
 
     __tablename__ = 'states'
-    id = Column(Integer, nullable=False, primary_key=True)
+    id = Column(Integer, nullable=False, primary_key=True, unique=True,
+                autoincrement=True)
     name = Column(String(128), nullable=False)
