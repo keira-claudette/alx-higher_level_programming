c160d0c21f9349c909f363311bca2a7a79e0a178
keira-claudette <claudette.keira@gmail.com>
2021-08-07 21:02:55 +0000

    explicitly added host and port to MySQLdb.connect argument

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
     import MySQLdb
     import sys
 
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
 
     cursor = db.cursor()
 
