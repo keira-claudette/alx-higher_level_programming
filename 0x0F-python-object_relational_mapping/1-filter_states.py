23b63d1bf8ff6f5e847ea74bf1837bd6f847c2a2
keira-claudette <claudette.keira@gmail.com>
2021-08-13 03:49:59 +0000

    returned n and N instead of just N upper

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    sql = """SELECT * FROM states WHERE name LIKE BINARY 'N%'\
+    sql = """SELECT * FROM states WHERE name LIKE BINARY 'N%'
     ORDER BY states.id ASC"""
 
     cursor.execute(sql)
