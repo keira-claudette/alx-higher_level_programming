f84eb84b3ae90a8502247f6e9ce6014c271d2265
keira-claudette <claudette.keira@gmail.com>
2021-08-19 18:13:50 +0000

    rearranged flags

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # takes in a URL as an argument, sends a GET request to the URL,
 # and displays the body of the response
-curl -s "$1" -X GET -H "X-HolbertonSchool-User-Id: 98"
+curl -sH "X-HolbertonSchool-User-Id: 98" -X GET "$1"
