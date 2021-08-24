02205249c35bb729d694209abc66d11a01233fb6
keira-claudette <claudette.keira@gmail.com>
2021-08-24 10:48:23 +0000

    ConnectionRefusedError: [Errno 111] Connection Refused

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,16 +3,14 @@
  value of the X-Request-Id variable found in the header of the response."""
 
 
-import urllib.request
-import urllib.parse
-import sys
-
-value = {'email': sys.argv[2]}
-data = urllib.parse.urlencode(value)
-data = data.encode('ascii')
-req = urllib.request.Request(sys.argv[1], data)
-with urllib.request.urlopen(req) as response:
-    print(response.read().decode("utf-8", "replace"))
-
 if __name__ == "__main__":
-    pass
+    import urllib.request
+    import urllib.parse
+    import sys
+
+    value = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf-8", "replace"))
