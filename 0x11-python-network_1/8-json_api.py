5847500666414b975e186578236c03b672c1adc5
keira-claudette <claudette.keira@gmail.com>
2021-08-24 11:04:26 +0000

    Checked is __name__ is equal to __main__

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,16 +3,17 @@
 http://0.0.0.0:5000/search_user with the letter as a parameter"""
 
 
-import requests
-import sys
+if __name__ == "__main__":
+    import requests
+    import sys
 
-q = sys.argv[1] if len(sys.argv) > 1 else ""
-r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
-try:
-    dic = r.json()
-    if dic == {}:
-        print('No result')
-    else:
-        print("[{}] {}".format(dic.get('id'), dic.get('name')))
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        dic = r.json()
+        if dic == {}:
+            print('No result')
+        else:
+            print("[{}] {}".format(dic.get('id'), dic.get('name')))
     except ValueError:
         print('Not a valid JSON')
