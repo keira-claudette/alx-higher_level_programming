d641b965a9686530baad67fde68e865163c8635c
keira-claudette <claudette.keira@gmail.com>
2021-08-24 10:59:01 +0000

    Checked is __name__ is equal to __main__

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,11 +2,13 @@
 """Takes in a URL, sends a request to the URL and
 displays the body of the response"""
 
-import requests
-from sys import argv
 
-res = requests.get(argv[1])
-if res.status_code >= 400:
-    print("Error code: {}".format(res.status_code))
-else:
-    print(res.text)
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    res = requests.get(argv[1])
+    if res.status_code >= 400:
+        print("Error code: {}".format(res.status_code))
+    else:
+        print(res.text)
