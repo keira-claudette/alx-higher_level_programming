37848b8d9977f03b7a7da1b7970f021488390919
keira-claudette <claudette.keira@gmail.com>
2021-08-24 10:51:14 +0000

    checked value of __name__

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,15 +3,14 @@
 takes in a URL, sends a request to the URL and displays the body of
  the response (decoded in utf-8)."""
 
-import sys
-import urllib.request
-import urllib.error
 
-req = urllib.request.Request(sys.argv[1])
-try:
-    urllib.request.urlopen(req)
-except urllib.error.HTTPError as e:
-    print("Error code: {}".format(e.code))
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+    import urllib.error
 
-if __name__ == "main":
-    pass
+    req = urllib.request.Request(sys.argv[1])
+    try:
+        urllib.request.urlopen(req)
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
