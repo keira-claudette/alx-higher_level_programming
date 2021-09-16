921b70eed1f04637d4fa54e01e1895f083fada8d
keira-claudette <claudette.keira@gmail.com>
2021-09-16 07:28:27 +0000

    solved semi-standard error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,4 +2,4 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', function (err, result) { if (err) { console.log(err);} else {console.log(result);} });
+fs.readFile(process.argv[2], 'utf-8', function (err, result) { if (err) { console.log(err); } else { console.log(result); } });
