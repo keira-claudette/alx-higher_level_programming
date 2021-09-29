6dccac09af8821edb05a7a2609abee657fac1943
Claudette Mokeira <68082012+keira-claudette@users.noreply.github.com>
2021-09-29 09:42:16 +0300

    Added on click event

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,1 +1,4 @@
-$('div#red_header').css('color', '#FF0000');
+/*updates the text color of the <header> element to red (#FF0000) when the user clicks on the tag DIV#red_header*/
+$("div#red_header").click(function() {
+    $("div#red_header").css("color", "#FF0000");
+});
